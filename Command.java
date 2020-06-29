fdc0931d88b00dc7275f6a946afd9b4d75caf5ce
grigoriev117 <38976132+grigoriev117@users.noreply.github.com>
2020-06-29 13:14:20 +0700

    Update Command.java

--- Command.java
+++ Command.java
@@ -3,7 +3,18 @@ import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import Exceptions.EndOfFileException;
 import Exceptions.IncorrectFileNameException;
-import java.util.*;
+import java.util.Date;
+import java.util.LinkedList;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import java.util.Collections;
 /**
  * Класс для обработки команд, вводимых в консоли
  */
@@ -11,8 +22,10 @@ import java.util.*;
 public class Command {
     /**
      * Обработка команд
+     * @throws IOException 
+     * @throws NumberFormatException 
      */
-    public static boolean switcher(CommandReader reader, Collection c, String s1, String s2) throws EndOfFileException {
+    public static boolean switcher(CommandReader reader, Collection c, String s1, String s2) throws EndOfFileException, NumberFormatException, IOException {
         switch (s1) {
             case ("help"):
                 help();
@@ -37,6 +50,9 @@ public class Command {
                 break;
             case ("save"):
                 save(c);
+                break;
+            case ("upload"):
+                upload(reader, c, s2);
                 break;
             case ("execute_script"):
                 return executeScript(c, s2);
@@ -65,8 +81,10 @@ public class Command {
         }
         return true;
     }
-    
-    
+        
+ interface Comparable<T> extends java.lang.Comparable<T> {
+
+    }  
 
 
 public static Checker<Boolean> boolCheck = (Boolean B) -> {
@@ -108,6 +126,7 @@ public static Checker<Boolean> boolCheck = (Boolean B) -> {
             }
         else  {
             System.out.println("\u001B[31m" + "В коллекции нет элементов" + "\u001B[0m");}
+        Collections.sort(c.list);
     }
       //  head : вывести первый элемент коллекции
         public static void head(Collection c) throws EndOfFileException {
@@ -166,6 +185,8 @@ public static Checker<Boolean> boolCheck = (Boolean B) -> {
                  * remove_all_by_weapon_type
                  */
                 public static void remove_all_by_weapon_type(CommandReader reader, Collection c, String s) {
+        if (s.equals("HEAVY_BOLTGUN") || s.equals("BOLT_RIFLE") || s.equals("PLASMA_GUN") || s.equals("COMBI_PLASMA_GUN") || s.equals("INFERNO_PISTOL"))
+        {
 		int size = c.list.size();
         	int i = 0;
 		List<Integer> num = new ArrayList<Integer>();
@@ -181,6 +202,11 @@ public static Checker<Boolean> boolCheck = (Boolean B) -> {
 		{
 		c.list.remove(c.list.get(num.get(j)));
                   }
+		Collections.sort(c.list);
+        }
+        else {
+        	System.out.println("\u001B[32m" + "Такого типа оружия пока нет!" + "\u001B[32m");
+        	}
                 }
 
             
@@ -195,11 +221,73 @@ public static Checker<Boolean> boolCheck = (Boolean B) -> {
                             	System.out.println(sm.toString());;
                         }
                     }
-                    // Collections.sort(c.list);
+                    Collections.sort(c.list);
                 }
                 
                 
-               
+             public static void upload(CommandReader reader, Collection c, String s) throws EndOfFileException, NumberFormatException, IOException {
+    	BufferedReader reader1 = new BufferedReader(new FileReader(s));
+        // считываем построчно
+        String line = null;
+        Scanner scanner = null;
+        int index = 0;
+        int cx = 1;
+        Double cy = 1.0;
+        String n1 = null;
+        String n2 = null;
+ 
+        while ((line = reader1.readLine()) != null) {
+        	SpaceMarine sm = new SpaceMarine();
+            scanner = new Scanner(line);
+          
+            
+            scanner.useDelimiter(",");
+            while (scanner.hasNext()) {
+                String data = scanner.next();
+                if (index == 0)
+                    sm.setId(Long.parseLong(data));
+                else if (index == 1)
+                    sm.setName(data);
+                else if (index == 2)
+                	cx = Integer.parseInt(data.replace("Coordinates{x=", ""));
+                else if (index == 3) {
+                	cy = Double.parseDouble(data.replace("y=", "").replace("}", ""));
+                    sm.setCoordinates(new Coordinates(cx, cy)); }
+                else if (index == 4) {
+                	String aa = data;
+                	LocalDate creationTime = LocalDate.now();
+                    sm.setCreationDate(creationTime);
+                }
+                else if (index == 5)
+                	sm.setHealth(Double.parseDouble(data));
+                else if (index == 6)
+                	sm.setLoyal(Boolean.parseBoolean(data));
+                else if (index == 7)
+                	sm.setAchievements(data);
+                else if (index == 8) {
+                	String weaponType1 = data;
+                	if (weaponType1.equals("HEAVY_BOLTGUN") || weaponType1.equals("BOLT_RIFLE") || weaponType1.equals("PLASMA_GUN") || weaponType1.equals("COMBI_PLASMA_GUN") || weaponType1.equals("INFERNO_PISTOL"))
+                    {
+                    	sm.setWeaponType(SpaceMarine.Weapon.valueOf(weaponType1));
+                    }
+                }
+                else if (index == 9)
+                	n1 = data.replace("Chapter{name=", "");
+                else if (index == 10) {
+                	n2 = data.replace("parentLegion=", "").replace("}", "");
+                	sm.setChapter(new Chapter(n1, n2));
+                }
+                else
+                    System.out.println("/");
+                index++;
+            }
+            index = 0;
+            c.list.add(sm);
+        }
+         
+        //закрываем наш ридер
+        reader.close();
+    }      
                 
     /**
      * Вывести в стандартный поток вывода информацию о коллекции
@@ -229,6 +317,7 @@ public static Checker<Boolean> boolCheck = (Boolean B) -> {
     public static void add(CommandReader reader, Collection c, String s) throws EndOfFileException {
         Long id = c.getRandId();
         c.list.add(toAdd(reader, id, s));
+        Collections.sort(c.list);
        
     }
     
@@ -250,7 +339,10 @@ public static Checker<Boolean> boolCheck = (Boolean B) -> {
         }
         String name = reader.handlerS("Введите String: name", SpaceMarine.nameCheck);
         c.list.set(c.list.indexOf(r), toAdd(reader, id, name));
-        // Collections.sort(c.list);
+        
+        Collections.sort(c.list);
+
+        
     }
     
     /**
@@ -269,7 +361,7 @@ public static Checker<Boolean> boolCheck = (Boolean B) -> {
             return;
         }
         c.list.remove(r);
-        // Collections.sort(c.list);
+        Collections.sort(c.list);
 
     }
     
@@ -287,11 +379,13 @@ public static Checker<Boolean> boolCheck = (Boolean B) -> {
         c.list.clear();
     }
     
-    /**
+     /**
      * Считывает и исполняет скрипт из указанного файла.
      * В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме
+     * @throws IOException 
+     * @throws NumberFormatException 
      */
-    public static boolean executeScript(Collection c, String s) {
+    public static boolean executeScript(Collection c, String s) throws NumberFormatException, IOException {
         boolean programIsWorking = true;
         //Reader reader;
         try (Reader reader = new Reader(s)) {
